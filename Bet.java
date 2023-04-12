fcef185a6b0dce9a2a8d8f8938774167e5f42810
DmitriyKrivoruchko <68115186+DmitriyKrivoruchko@users.noreply.github.com>
2023-04-12 12:57:17 +0200

    Update Bet.java
    Verbessert

--- Bet.java
+++ Bet.java
@@ -1,5 +1,7 @@
 package racingSnail;
 
+import java.util.logging.Logger;
+
 /**
  * Class to define a bet
  * @author d.krivoruchko
@@ -7,27 +9,32 @@ package racingSnail;
 public class Bet {
 
     /** Player's name */
-    private String playerName;
+    private final String playerName;
 
     /** Snail's name */
-    private String snailName;
+    private final String snailName;
 
     /** Amount to bet */
     private double betAmount;
 
 
     /**
+     * Logger object is used to log messages
+     */
+    Logger logger = Logger.getLogger(String.valueOf(Racing.class));
+
+
+    /**
      * Constructor of a bet
-     * @param playerName - Name of a player who bets
-     * @param snailName - Name of a snail to bet on
-     * @param betAmount - Amount to bet
-     * @throws IllegalArgumentException - if the bet amount less than 0.01
+     * @param playerName name of a player who bets
+     * @param snailName name of a snail to bet on
+     * @param betAmount amount to bet
      */
     public Bet (String playerName, String snailName, double betAmount) {
         this.playerName = playerName;
         this.snailName = snailName;
         if (betAmount < .01) {
-            throw new IllegalArgumentException("You have to bet at least .01 Euro");
+            logger.info("You have to bet at least .01 Euro");
         } else {
             this.betAmount = betAmount;
         }
@@ -36,7 +43,7 @@ public class Bet {
 
     /**
      * Method returns information about a bet in a String-form
-     * @return - Player's name, snail's name to bet on and a bet amount to bet
+     * @return player's name, snail's name to bet on and a bet amount to bet
      */
     public String toString() {
         return this.playerName+" bet on "+this.snailName+" "+this.betAmount+"!";
@@ -45,22 +52,21 @@ public class Bet {
 
     /**
      * Method returns player's name in a String-form
-     * @return - Name of a player
+     * @return name of a player
      */
     public String getPlayerName() { return playerName; }
 
 
     /**
      * Method returns snail's name in a String-form
-     * @return - Name of a snail
+     * @return name of a snail
      */
     public String getSnailName() { return snailName; }
 
 
     /**
      * Method returns the bet amount
-     * @return - Bet amount
+     * @return bet amount
      */
     public double getBetAmount() { return betAmount; }
-
 }
