ea87a575be84764a24ce0a2eaba8b35106cfec4e
DmitriyKrivoruchko <68115186+DmitriyKrivoruchko@users.noreply.github.com>
2023-04-12 12:55:37 +0200

    Update RacingSnail.java
    Verbessert

--- RacingSnail.java
+++ RacingSnail.java
@@ -9,13 +9,13 @@ import java.util.Random;
 public class RacingSnail {
 
     /** Racing snail's name */
-    private String name;
+    private final String name;
 
     /** Racing nail's race */
-    private String race;
+    private final String race;
 
     /** Racing snail's maximal speed */
-    private Double maxSpeed;
+    private final int maxSpeed;
 
     /** Covered path */
     private double covered;
@@ -23,11 +23,11 @@ public class RacingSnail {
 
     /**
      * Constructor of racing snails
-     * @param name - Racing snail's name
-     * @param race - Racing snail's race
-     * @param maxSpeed - Racing snail's maximal speed
+     * @param name Racing snail's name
+     * @param race Racing snail's race
+     * @param maxSpeed Racing snail's maximal speed
      */
-    public RacingSnail(String name, String race, double maxSpeed) {
+    public RacingSnail(String name, String race, int maxSpeed) {
         this.name = name;
         this.race = race;
         this.maxSpeed = maxSpeed;
@@ -39,16 +39,14 @@ public class RacingSnail {
      * Method for a snail to crawl
      */
     public void crawl() {
-      //  Random random = new Random();
-        //TODO die Formel ist meiner meinung nach nicht korrekt
-        //"Sie kriecht eine zufällige Strecke größer null und kleiner ihrer Maximalgeschwindigkeit. Nimm als Zeiteinheit 1 an."
-        this.covered +=new Random().nextInt(maxSpeed.intValue());
+        Random random = new Random();
+        this.covered += maxSpeed*random.nextFloat()*1;
     }
 
 
     /**
      * Method returns the name of a snail
-     * @return - The name of a snail
+     * @return the name of a snail
      */
     public String getName() {
         return name;
@@ -56,38 +54,17 @@ public class RacingSnail {
 
 
     /**
-     * Method returns the race of a snail
-     * @return - The Race  of a snail
-     */
-    //TODO brauch man nicht
-    public String getRace() {
-        return race;
-    }
-
-
-    /**
-     * Method returns the maximum speed of a snail
-     * @return - The maximum speed of a snail
-     */
-    //TODO brauch man nicht bzw. wird nirgends genutzt
-    public double getMaxSpeed() {
-        return maxSpeed;
-    }
-
-
-    /**
      * Method returns the covered distance
-     * @return - The covered distance
+     * @return the covered distance
      */
     public double getCovered() { return covered; }
 
 
     /**
      * Method to get the information about a racing snail in the String-form
-     * @return - The information about racing snail's parameters
+     * @return the information about racing snail's parameters
      */
     public String toString() {
         return "Racing snail's name is "+this.name+" from "+this.race+" with maximal speed "+this.maxSpeed+".";
     }
-
 }
